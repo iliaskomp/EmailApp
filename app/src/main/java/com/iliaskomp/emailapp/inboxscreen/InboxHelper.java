97b90ab804feca21f1c1dae8f568b76e816d1f4b
IliasKomp <iliaskomp@gmail.com>
2017-03-16 22:21:50 +0100

    Fixed subject line in inbox view

--- app/src/main/java/com/iliaskomp/emailapp/inboxscreen/InboxHelper.java
+++ app/src/main/java/com/iliaskomp/emailapp/inboxscreen/InboxHelper.java
@@ -5,10 +5,10 @@ package com.iliaskomp.emailapp.inboxscreen;
  */
 
 public class InboxHelper {
-    private static final int MESSAGE_INBOX_LENGTH = 25;
+    private static final int MESSAGE_INBOX_LENGTH = 45;
 
     public static String formatMessageShortForInbox(String message) {
-        return message.length() <= MESSAGE_INBOX_LENGTH
-                ? message : message.substring(0, MESSAGE_INBOX_LENGTH);
+        return message.trim().length() <= MESSAGE_INBOX_LENGTH
+                ? message.trim() : message.substring(0, MESSAGE_INBOX_LENGTH).trim();
     }
 }
